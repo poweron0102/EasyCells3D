00f765c420f29101b35bd7c5bf569a7db8959037
poweron0102 <nathanpinheiro99@gmail.com>
2025-09-06 09:50:04 -0300

    Add camera movement and rotation controls to the game

--- Levels/test.py
+++ Levels/test.py
@@ -1,11 +1,15 @@
 from EasyCells3D import Game
 from EasyCells3D.Components import Camera, Item
 from EasyCells3D.Components.SphereHittable import SphereHittable
-from EasyCells3D.Geometry import Vec3
+from EasyCells3D.Geometry import Vec3, Quaternion
+
+import pygame as pg
 
 bola: Item
+camera: Item
 
 def init(game: Game):
+    global camera
     camera = game.CreateItem()
     camera.AddComponent(Camera(aspect_ratio=1.0))
 
@@ -14,7 +18,37 @@ def init(game: Game):
     bola.AddComponent(SphereHittable(0.5))
     bola.transform.position += Vec3(0, 0, 2)
 
+    bola = game.CreateItem()
+    bola.AddComponent(SphereHittable(0.5))
+    bola.transform.position += Vec3(1, 0, 2)
+
+    bola = game.CreateItem()
+    bola.AddComponent(SphereHittable(0.5))
+    bola.transform.position += Vec3(-1, 0, 2)
+
 
 
 def loop(game: Game):
-    bola.transform.position += Vec3(1, 0, 0)
+    #bola.transform.position += Vec3(0.1, 0, 0)
+
+    offset: Vec3[float] = Vec3(0, 0, 0)
+    if pg.key.get_pressed()[pg.K_w]:
+        offset += Vec3(0, 0, 0.1)
+    if pg.key.get_pressed()[pg.K_s]:
+        offset += Vec3(0, 0, -0.1)
+    if pg.key.get_pressed()[pg.K_a]:
+        offset += Vec3(-0.1, 0, 0)
+    if pg.key.get_pressed()[pg.K_d]:
+        offset += Vec3(0.1, 0, 0)
+    camera.transform.position += offset
+
+    rotation = Vec3(0, 0, 0)
+    if pg.key.get_pressed()[pg.K_UP]:
+        rotation += Vec3(-1, 0, 0)
+    if pg.key.get_pressed()[pg.K_DOWN]:
+        rotation += Vec3(1, 0, 0)
+    if pg.key.get_pressed()[pg.K_LEFT]:
+        rotation += Vec3(0, -1, 0)
+    if pg.key.get_pressed()[pg.K_RIGHT]:
+        rotation += Vec3(0, 1, 0)
+    camera.transform.rotation = camera.transform.rotation * Quaternion.from_axis_angle(rotation.normalize(), 0.05)
